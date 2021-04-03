3d31c46142b2f5d8aedbd6491d2176c8f6bb9551
rishab <rishabkashyap14@gmail.com>
2021-04-03 12:51:18 +0530

    Roughwork

---
+++ rough/Web_api_with_cpp/smart_library_frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import CalculatorButton from './CalculatorButton'
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+	  <CalculatorButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
